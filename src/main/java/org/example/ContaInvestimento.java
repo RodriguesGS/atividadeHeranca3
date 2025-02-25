9c8f59c5e3d6bff3dfbf4e18ade19ffc5822f792
Gabriel <rodriguesgasoares@gmail.com>
2025-02-25 12:41:28 -0300

    feat: add contaInvestimento

--- src/main/java/org/example/ContaInvestimento.java
+++ src/main/java/org/example/ContaInvestimento.java
@@ -1,4 +1,20 @@
 package org.example;
 
-public class ContaInvestimento {
+public class ContaInvestimento extends ContaBancaria {
+    public ContaInvestimento(int numeroConta, String titular, double saldo) {
+        super(numeroConta, titular, saldo);
+    }
+
+    public void sacar(double valor) {
+        try {
+            double taxa = valor * 0.02;
+            double total = valor + taxa;
+
+            if (total > getSaldo())
+                throw new IllegalArgumentException("Saldo insuficiente!");
+            setSaldo(getSaldo() - total);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
